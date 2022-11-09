4fee6903eaaca4a3480278e3a6bff5fde20f5189
Gyabi <buyuuka12345@gmail.com>
2022-11-09 23:55:00 +0900

    マテリアルデザイン適応

--- TODOManager/TODOManager/Presentation/Views/MainWindow.xaml.cs
+++ TODOManager/TODOManager/Presentation/Views/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System.Windows;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
 
 namespace TODOManager.Presentation.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
