a546c868b1d408e0494f41cc823498e48add6276
Gyabi <buyuuka12345@gmail.com>
2022-11-16 17:56:19 +0900

    依存関係プロパティを導入

---
+++ TODOManager/TODOManager/Presentation/ViewModels/Contents/TodoItemVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TODOManager.Domain.DomainModel;
+
+namespace TODOManager.Presentation.ViewModels.Contents
+{
+    /// <summary>
+    /// VM内で利用するドメインを加工したクラス
+    /// </summary>
+    public class TodoItemVM
+    {
+        /// <summary>
+        /// TodoItemからコンストラクタを起動する
+        /// </summary>
+        /// <param name="todoitem"></param>
+        public TodoItemVM(TodoItem todoitem)
+        {
+
+        }
+    }
+}
--- TODOManager/TODOManager/Presentation/Views/Parts/TodoItemCard.xaml.cs
+++ TODOManager/TODOManager/Presentation/Views/Parts/TodoItemCard.xaml.cs
@@ -13,17 +13,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TODOManager.Domain.DomainModel;
 
 namespace TODOManager.Presentation.Views
 {
     /// <summary>
-    /// UserControl1.xaml の相互作用ロジック
+    /// TodoItemCard.xaml の相互作用ロジック
     /// </summary>
     public partial class TodoItemCard : UserControl
     {
         public TodoItemCard()
         {
             InitializeComponent();
+        }
+
+        //依存関係プロパティ
+        public static readonly DependencyProperty ItemProperty =
+            DependencyProperty.Register(
+                nameof(Item), //プロパティ名
+                typeof(TodoItem), //バインドするデータの型
+                typeof(TodoItemCard), //自分自身の型
+                new PropertyMetadata(null) //初期値
+        );
+
+        public TodoItem Item
+        {
+            get => (TodoItem)GetValue(ItemProperty);
+            set => SetValue(ItemProperty, value);
         }
     }
 }
